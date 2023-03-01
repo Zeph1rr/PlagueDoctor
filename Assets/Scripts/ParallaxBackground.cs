5507cade0eb2731ee8e005623f7ece70b51a16af
Zeph1rr <grianton535@gmail.com>
2023-03-01 13:12:30 +0300

    Initial

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+
+public class ParallaxBackground: MonoBehaviour
+{
+    [SerializeField] private float _parallaxFactor = 0.5f;
+    [SerializeField] private Transform _followTarget;
+
+    private Vector3 _lastFollowTargetPosition;
+    private Vector3 _deltaFollowTargetPosition;
+
+    private void Start()
+    {
+        _lastFollowTargetPosition = _followTarget.position;
+    }
+
+    private void LateUpdate()
+    {
+        _deltaFollowTargetPosition = _followTarget.position - _lastFollowTargetPosition;
+
+        transform.position += _deltaFollowTargetPosition * _parallaxFactor;;
+
+        _lastFollowTargetPosition = _followTarget.position;
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+﻿# Augustin Dias: Plague Doctor
+
+## About
+
+- UnityEngine: 2021.3.19f1
+- Author: Anton <Zeph1rr> Grigoryev
+- Artist: Anastasia Erohina https://vk.com/erohina_art_2022
